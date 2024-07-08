d973cbaa2b8a06f9cccded6622e43673c3cc38e1
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-08 15:35:22 +0300

    Writing 'Clean' functions

---
+++ 7 Functions/7.8 Writing 'Clean' functions.js
@@ -0,0 +1,10 @@
+/*
+Let's learn some principles for writing functions:
+
+Use descriptive function names. Your function's name should be able to explain exactly what the function does and it's purpose. The more specific the job, the more specific the name should be.
+A function should do one thing only. If you have a giant function that executes multiple algorithms and calculations, you should split it into smaller functions. Every function should be as small as it possibly can.
+Keep your functions small. As we already stated, functions should be kept as small as possible. Some programmers even state that no function should be larger than 20 - 25 lines of code.
+Use as little parameters as possible. Try to avoid using booleans as arguments as well. The less arguments, the more readability increases.
+When using functions, you should be able to read your code from top to bottom. Every function that calls another function should be above the same function. As the father of clean code (Robert C. Martin) states:
+"We want the code to read like a top-down narrative. We want every function to be followed by those at the next level of abstraction so that we can read the program, descending one level of abstraction at a time as we read down the list of functions. I call this The Stepdown Rule. To say this differently, we want to be able to read the program as though it were a set of TO paragraphs, each of which is describing the current level of abstraction and referencing subsequent TO paragraphs at the next level down."
+*/
