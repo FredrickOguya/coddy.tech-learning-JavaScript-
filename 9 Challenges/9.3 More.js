5b34c828a89cc251823f62c2c8309ace30c1578c
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-10 15:12:54 +0300

    9.3 Mario(More).js

---
+++ 9 Challenges/9.3 More.js
@@ -0,0 +1,38 @@
+/*
+Mario (More)
+
+It is recommended that you finish Mario (Less) before moving onto this challenge.
+
+In this challenge we will also recreate pyramids from the video game mario. 
+If you have ever played that game, you might have noticed two pyramids facing each other. 
+In this challenge we will have to recreate those pyramids. For example:
+
+    #  #
+   ##  ##
+  ###  ###
+ ####  ####
+#####  #####
+These pyramids have a height of 5. The height will be anywhere from 1 to 8. 
+You are given the height of these pyramids in the program. 
+Your job is to use the height to print these two pyramids facing away from each other with 2 spaces in 
+between. Remember, every time you print in JavaScript, you transfer to a new line. 
+To avoid that, you should create a string with spaces and dashes of the row and then print the string.
+
+
+Challenge
+
+Hard
+You are given a function called pyramids that takes in a parameter called height. 
+You need to complete the code inside the function to make it print pyramids as shown above. 
+A pyramid of height 3 would look like:
+
+  #  #
+ ##  ##
+###  ###
+
+Hints
+Your code should work the same as the code for Mario (Less), 
+just after the spaces and dashes for the first pyramid, you need to add two spaces 
+to the string, and then i dashes again. If you have not done Mario (Less) yet, please 
+do so before you continue.
+*/
