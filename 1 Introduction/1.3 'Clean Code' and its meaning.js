a65343e8c7a08c3c0eac63929ebfbb1f0f293c42
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-13 20:33:33 +0300

    1.3 'Clean Code' and its meaning

---
+++ 1 Introduction/1.3 'Clean Code' and its meaning.js
@@ -0,0 +1,21 @@
+/*
+Imagine you are a professional developer in a team of 15 developers. You are all working on one website. 
+What would happen if every developer started writing code with their own principles and in their own 
+distinct way? Chaos, of course! That's why it's important not just to write code, but to write clean code.
+
+Clean code is code that is easy to read and understand. Any developer should be able to look at your code, 
+read it, understand it easily and be able to continue it. As a beginner don't stress too much about this,
+ but it's better to start early. During the course you will learn some principles that will help you make 
+ sure your code is always clean. Believe it or not, you can already learn some principles:
+
+ 
+
+Keep you code DRY
+By DRY, we mean Don't Repeat Yourself. You will learn more about this later.
+
+Use semicolons
+In most programming languages, at the end of every line of code you have to put a semicolon ;. 
+In JavaScript, it is not necessary to put a semicolon at the end of lines, but it is a good practice 
+to put them. Some developers choose not to. Whatever you choose, stick to one and don't mix the two.
+
+*/
