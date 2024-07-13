afd1e0eb8b14066c55a9913ef8fde217742fe10e
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-13 21:04:43 +0300

    Writing 'Clean' variables

---
+++ 2 Variables & Data/2.6 Writing 'Clean' variables.js
@@ -0,0 +1,13 @@
+/*
+There are some principles to keep in mind when declaring and working with variables.
+
+Here are some to keep in mind:
+
+Only declare variables when needed. This doesn't mean that you should rarely use variables, it just means don't over do it. Declare them when and where is needed only.
+
+Use descriptive names for variables that describe the piece of data they hold. For example, instead of writing let a1 = '123456', you should write let badPassword = '123456'.
+
+Declare variables where you need them. Some schools these days teach you that all variables should be declared at the top of the program. That is not good practice. Declare a variable as close to the lines where you use it as possible.
+
+Use const unless you know the value will change. For variables that you will only use once, or only in a specific place in the program and you know that you won't have to change their value, use const to declare them. Only use let if the value of the variable has to change during the program.
+*/
