0a298d684e99ceb27ae18de04699b84a184b7f7d
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-13 20:31:29 +0300

    1.2 Hello world!

---
+++ 1 Introduction/1.2 Hello World.js
@@ -0,0 +1,19 @@
+/*
+Hello World!
+
+In the programming world, once a new apprentice wants to learn the magic of code, they are introduced with a program called "Hello, World!". This is one of the most simple programs you can write with any language.
+
+The program consists of printing "Hello, World!" to the screen. In JavaScript, we can print words, numbers and data with the command console.log().
+
+For example, if we want to print "Hello, World!" we would write:
+
+console.log('Hello, World!');
+When we want to print text, we have to write the text in the parentheses of console.log(). We also have to wrap the text with ' ' or " " so the compiler that runs the program knows that the text we wrote isn't code, but actual text we want to print.
+
+
+Challenge
+
+Easy
+Now, it's your turn. Write the command and press "Run Code". Remember, the program has to print EXACTLY "Hello, World!" in order to be correct.
+*/
+console.log('Hello, World!');
