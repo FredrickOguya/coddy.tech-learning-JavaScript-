666aa9ebce6838e474b74d813814dc26bda0d8dc
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-13 20:40:51 +0300

    Data types

---
+++ 2 Variables & Data/2.2 Data types.js
@@ -0,0 +1,81 @@
+/*
+There are lots of types of data you will encounter while programming. 15, 3.1415, true, 'Hello, World!' are all different types of data.
+
+In JavaScript, we declare all variables with let and const, but it's still very valuable to know the different data types:
+
+Number
+We have already worked with numbers. There are integers, which are whole numbers without decimals. Floats are numbers with decimal points. In JavaScript, all types of numbers are just called Number.
+
+let age = 41;
+String
+A string is what we usually call text. An example for a string would be: 'Hi, my name is Jack!', or 'teleportation123'. A single character is called a char: 'a'. For example:
+
+let name = 'John';
+const lastName = 'Doe';
+let myChar = 'A';
+Boolean
+Booleans have two values. True or false. Booleans are like a switch that can be turned on or off. For example:
+
+let isFriday = true;
+let isTwoOClock = false;
+Undefined & Null
+Undefined is a variable that has not been assigned a value: let myVariable;
+
+Null is an intentional empty value: let unknownValue = null;
+
+ 
+
+There are two more types of data called Objects and Arrays. We will cover Arrays later in this course. If you want to learn about Objects, which are very important in JavaScript, you can check out my next course called Object Oriented Programming in JavaScript.
+
+ 
+
+
+Challenge
+
+Easy
+Create variables of each data type. They should be named myNumber, myString, myBoolean, myUndefined, and myNull. You do not have to print them, just declare them.
+
+
+Hints
+
+Hint 1
+
+
+To declare a number, you have to write:
+
+ let myNumber = anyNumber
+Hint 2
+
+
+To declare a string you have to write
+
+let myString = 'anyText';
+Hint 3
+
+
+To declare a boolean you have to write
+
+let myBoolean = true
+Hint 4
+
+
+To declare an undefined variable, you have to write
+
+let myUndefined;
+Hint 5
+
+
+For null, you have to give it a null value
+
+let myNull = null;
+*/
+//Solution
+
+
+let myNumber = 12;
+let myString = 'hello';
+let myBoolean = true;
+let myUndefined;
+let myNull = null;
+
+
