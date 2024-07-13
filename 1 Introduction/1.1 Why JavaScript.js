feb8d5fcee5a1a6b283c9d7fba411a091f34fc0d
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-13 20:29:31 +0300

    1.1 Why JavaScript

---
+++ 1 Introduction/1.1 Why JavaScript.js
@@ -0,0 +1,15 @@
+/*
+Today, programming has grown in to one of the biggest career fields out there. Countless languages, frameworks, libraries and other extensions are created every day! So, out of all the options, why should you choose JavaScript? JavaScript is a programming language that is one of the core technologies of the World Wide Web.
+
+ 
+
+Uses
+Web Development: JavaScript is used to create dynamic and interactive web content like applications and browsers. JavaScript is the most used programming language in the world, used as a client-side programming language by 97.0% of all websites. The push of a button, the changing of colors, every click, every like and every comment on every website is possible thanks to JavaScript.
+
+Game Development: JavaScript is frequently used to create in-browser games. Developers use JavaScript to create 2D and 3D puzzles, role-playing games, racing games, platform games, and more.
+
+ 
+
+What will you learn?
+In this course, you will learn the fundamentals of the most modern version of JavaScript. We will start from the most basic concepts and move on to more complicated topics. We will be working in the console for now, because making websites with JavaScript requires you to be fully familiar with the fundamentals of coding. Once you learn those, you can move on to the next courses which teach JavaScript more in depth and teach you how to use it in the browser to make websites. Have fun and take your time!
+*/
