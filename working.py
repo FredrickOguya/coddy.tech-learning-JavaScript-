6bb13916cd189db4e8353a8dc5b8171b6a1c8c19
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-31 12:32:37 +0300

    lists

--- working.py
+++ working.py
@@ -1,5 +1,13 @@
+"""
 inpu = 5
 no =1
 for i in range (1,inpu+1):
     no*=i
 print(no)
+"""
+def values(lst):
+    # Write code here
+    num = len(lst)
+    for i in range(num):
+        print(lst[i])
+values([5, 9, 2, 10, 2])
