7b81cc1437a5465369fa2c1d92211ebc0f68634d
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-08-01 13:16:27 +0300

    reverse

--- working.py
+++ working.py
@@ -11,7 +11,7 @@ def values(lst):
     for i in range(num):
         print(lst[i])
 values([5, 9, 2, 10, 2])
-"""
+
 def merge(lst1, lst2):
     # Write code here
     num = len(lst2)
@@ -19,3 +19,11 @@ def merge(lst1, lst2):
         lst2.append(lst1[i])
         lst2.sort()
         return lst2
+"""
+def reverse(lst):
+    # Write code here
+    new_lst  = []
+    for i in range(len(lst)-1,0,-1):
+        new_lst.append(lst[i])
+    print(new_lst)
+reverse([1, 2, 3])
